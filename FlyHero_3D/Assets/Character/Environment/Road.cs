650d24c17ced227d3e6849eaf50297c6a7b8abfe
upallnightcoding <upallnightcoding@gmail.com>
2022-09-12 02:51:52 -0400

    Installed Icons

--- FlyHero_3D/Assets/Character/Environment/Road.cs
+++ FlyHero_3D/Assets/Character/Environment/Road.cs
@@ -6,12 +6,12 @@ public class Road : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject road;
-    [SerializeField] private float depth;
+    [SerializeField] private int nForwardDepth;
+    [SerializeField] private GameObject blankCoin;
 
     private float lastPosition;
-
+    private float forwardDepth;
     private float roadSize;
-    
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +20,27 @@ public class Road : MonoBehaviour
 
         lastPosition = roadSize;
 
-        depth = roadSize;
+        forwardDepth = roadSize * nForwardDepth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.z + depth > lastPosition)
+        if ((player.transform.position.z + forwardDepth) > lastPosition)
         {
-            GameObject go = Instantiate(road);
-
             lastPosition += roadSize;
+
+            GameObject go = Instantiate(road);
 
             float x = 0.0f;
             float y = 2.15f;
             float z = lastPosition;
             go.transform.position = new Vector3(x, y, z);
+
+            x = 0.0f;
+            y = 4.0f;
+            z = lastPosition;
+            GameObject coin = Instantiate(blankCoin, new Vector3(x, y, z), Quaternion.identity, go.transform); 
         }
     }
 }
--- FlyHero_3D/Assets/Character/Model/Controller.cs
+++ FlyHero_3D/Assets/Character/Model/Controller.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Rigidbody rb;
+    public CharacterController cc;
     public float flySpeed = 20.0f;
-    public float yawAmount = 120.0f;
+    public float horzSpeed;
+    public float vertSpeed;
+
+    public float yawAmount = 100.0f;
 
     private float yaw;
+
+    float horizontalInput, verticalInput;
+    float oldHorizontalInput, oldVerticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = null;
+    }
+
+    private void Update()
+    {
+        //TranslateUpdate();
+        ControllerUpdate();
     }
 
     // Update is called once per frame
-    void Update()
+    void ControllerUpdate()
     {
-        transform.position += transform.forward * flySpeed * Time.deltaTime;
+        //transform.position += transform.forward * flySpeed * Time.deltaTime;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        cc.Move(transform.forward * flySpeed * Time.deltaTime);
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * horzSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxisRaw("Vertical") * vertSpeed * Time.deltaTime;
+
+        horizontalInput = Mathf.Lerp(oldHorizontalInput, horizontalInput, Time.deltaTime);
+        verticalInput = Mathf.Lerp(oldVerticalInput, verticalInput, Time.deltaTime);
 
         yaw += horizontalInput * yawAmount * Time.deltaTime;
         //yaw = Mathf.Clamp(yaw, -90.0f, 90.0f);
@@ -29,8 +48,37 @@ public class Controller : MonoBehaviour
         float pitch = Mathf.Lerp(0, 45, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
         float roll = Mathf.Lerp(0, 45, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
 
-        //transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
-        transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+        //rb.velocity = new Vector3(0.0f, 0.0f, 75.0f);
 
+        transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+        //transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+
+        oldHorizontalInput = horizontalInput;
+        oldVerticalInput = verticalInput;
+    }
+
+    void TranslateUpdate()
+    {
+        transform.position += transform.forward * flySpeed * Time.deltaTime;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * horzSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxisRaw("Vertical") * vertSpeed * Time.deltaTime;
+
+        horizontalInput = Mathf.Lerp(oldHorizontalInput, horizontalInput, Time.deltaTime);
+        verticalInput = Mathf.Lerp(oldVerticalInput, verticalInput, Time.deltaTime);
+
+        yaw += horizontalInput * yawAmount * Time.deltaTime;
+        //yaw = Mathf.Clamp(yaw, -90.0f, 90.0f);
+
+        float pitch = Mathf.Lerp(0, 45, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        float roll = Mathf.Lerp(0, 45, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
+
+        //Debug.Log($"Roll: {roll} / {horizontalInput}");
+
+        transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+        //transform.rotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch + Vector3.forward * roll);
+
+        oldHorizontalInput = horizontalInput;
+        oldVerticalInput = verticalInput;
     }
 }
